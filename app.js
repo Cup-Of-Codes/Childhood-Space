14c6c16189368d267e7d0dc908c740d439b38793
Saraaltaweel <s.bata97.st@gmail.com>
2021-02-28 22:26:04 +0200

    new changes

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-// var video = document.getElementById("myVideo");
-// var btn = document.getElementById("myBtn");
 
-// function myFunction() {
-//   if (video.paused) {
-//     video.play();
-//     btn.innerHTML = "Pause";
-//   } else {
-//     video.pause();
-//     btn.innerHTML = "Play";
-//   }
+// function UserName(name){
+//     this .name=name;
+  
 // }
+let user;
+function save(){
+    let saveName= JSON.stringify(user);
+    localStorage.setItem('userName',saveName);
+}
+let form =document.getElementById('form');
+form.addEventListener('submit',showResult);
+
+
+function showResult(event){
+    console.log(event);
+    event.preventDefault();
+   
+    user = event.target.name.value;
+    console.log(showResult);
+    save();
+    alert('welcome '+ user);
+    
+}
+
